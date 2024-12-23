acdbf7ce1fb03245108296eed5ddcb447b13fb8f
edwardowens <eddiebeme@gmail.com>
2024-12-22 20:22:09 -0800

    feat: Adding ability to create k3d clusters in test suites.

---
+++ k3d.go
@@ -0,0 +1,141 @@
+package ksuite
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/k3d-io/k3d/v5/cmd/util"
+	k3dcluster "github.com/k3d-io/k3d/v5/pkg/client"
+	"github.com/k3d-io/k3d/v5/pkg/config"
+	"github.com/k3d-io/k3d/v5/pkg/config/v1alpha5"
+	"github.com/k3d-io/k3d/v5/pkg/runtimes"
+	k3d "github.com/k3d-io/k3d/v5/pkg/types"
+	"github.com/k3d-io/k3d/v5/version"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
+	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+)
+
+func CreateCluster(ctx context.Context) (*Cluster, error) {
+	tag := version.K3sVersion
+	simpleClusterConfig := v1alpha5.SimpleConfig{
+		Image:   fmt.Sprintf("%s:%s", k3d.DefaultK3sImageRepo, tag),
+		Servers: 1,
+	}
+	simpleClusterConfig.Name = uuid.New().String()
+
+	simpleClusterConfig.Options.K3dOptions.Wait = true
+
+	registryPort := findFreePort(10562)
+
+	simpleClusterConfig.Registries.Create = &v1alpha5.SimpleConfigRegistryCreateConfig{
+		Name:     simpleClusterConfig.Name,
+		Host:     "0.0.0.0",
+		HostPort: strconv.Itoa(int(registryPort)),
+	}
+
+	exposeAPI := &k3d.ExposureOpts{}
+	var err error
+	if simpleClusterConfig.ExposeAPI.HostPort == "" {
+		exposeAPI, err = util.ParsePortExposureSpec("random", k3d.DefaultAPIPort)
+		if err != nil {
+			return nil, err
+		}
+	}
+	simpleClusterConfig.ExposeAPI = v1alpha5.SimpleExposureOpts{
+		Host:     exposeAPI.Host,
+		HostIP:   exposeAPI.Binding.HostIP,
+		HostPort: exposeAPI.Binding.HostPort,
+	}
+
+	// Expose the loadbalancer (allows for ingress)
+	ingressPort := findFreePort(9090)
+	simpleClusterConfig.Ports = append(simpleClusterConfig.Ports, v1alpha5.PortWithNodeFilters{
+		Port:        fmt.Sprintf("%d:80", ingressPort),
+		NodeFilters: []string{"loadbalancer"},
+	})
+
+	clusterConfig, _ := config.TransformSimpleToClusterConfig(ctx, runtimes.SelectedRuntime, simpleClusterConfig, "")
+
+	clusterConfig, _ = config.ProcessClusterConfig(*clusterConfig)
+
+	err = k3dcluster.ClusterRun(ctx, runtimes.SelectedRuntime, clusterConfig)
+	defer func() {
+		if err != nil {
+			_ = k3dcluster.ClusterDelete(ctx, runtimes.SelectedRuntime, &clusterConfig.Cluster, k3d.ClusterDeleteOpts{})
+		}
+	}()
+	if err != nil {
+		return nil, err
+	}
+
+	var conf *api.Config
+	conf, err = k3dcluster.KubeconfigGet(ctx, runtimes.SelectedRuntime, &clusterConfig.Cluster)
+	if err != nil {
+		return nil, err
+	}
+
+	out := &Cluster{
+		K3D:          &clusterConfig.Cluster,
+		RegistryAddr: fmt.Sprintf("localhost:%d", registryPort),
+		IngressPort:  ingressPort,
+	}
+
+	out.Context, out.Cancel = context.WithCancelCause(ctx)
+	context.AfterFunc(out.Context, func() {
+		_ = DeleteCluster(ctx, out)
+	})
+	out.KubeConfig = clientcmd.NewDefaultClientConfig(*conf, nil)
+	kubeConfDir := filepath.Join(os.TempDir(), simpleClusterConfig.Name)
+	out.KubeConfPath = filepath.Join(kubeConfDir, "kube_config")
+	err = clientcmd.WriteToFile(*conf, out.KubeConfPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+func DeleteCluster(ctx context.Context, cl *Cluster) error {
+	err := k3dcluster.ClusterDelete(ctx, runtimes.SelectedRuntime, cl.K3D, k3d.ClusterDeleteOpts{})
+	if err != nil {
+		return err
+	}
+
+	_ = os.Remove(cl.KubeConfPath)
+
+	cl.Cancel(nil)
+
+	return nil
+}
+
+type Cluster struct {
+	Context      context.Context
+	Cancel       context.CancelCauseFunc
+	KubeConfPath string
+	KubeConfig   clientcmd.ClientConfig
+	K3D          *k3d.Cluster
+	RegistryAddr string
+	IngressPort  uint16
+}
+
+func findFreePort(from uint16) uint16 {
+	for !isPortFree(int(from)) {
+		from++
+	}
+
+	return from
+}
+
+func isPortFree(port int) bool {
+	conn, _ := net.DialTimeout("tcp", ":"+strconv.Itoa(port), 3*time.Second)
+	if conn != nil {
+		defer conn.Close()
+		return false
+	}
+	return true
+}
---
+++ suite.go
@@ -0,0 +1,99 @@
+package ksuite
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/suite"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"slices"
+)
+
+var _ suite.SetupAllSuite = &KubeSuite{}
+var _ suite.TearDownAllSuite = &KubeSuite{}
+var _ suite.AfterTest = &KubeSuite{}
+
+type KubeSuite struct {
+	suite.Suite
+	Cluster *Cluster
+	Kube    kubernetes.Interface
+	Config  *rest.Config
+
+	// Don't touch the namespaces specified.
+	SkipCleanNamespaces []string
+	// Don't touch the namespaces that match the selectors.
+	SkipCleanNamespaceSelectors []string
+}
+
+func (k *KubeSuite) AfterTest(_, _ string) {
+	ctx := context.Background()
+	namespaces, err := k.Kube.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return
+	}
+
+	skipNamespaces := append([]string{
+		"default",
+		"kube-system",
+		"kube-public",
+	}, k.SkipCleanNamespaces...)
+
+	skipLabels := append([]string{}, k.SkipCleanNamespaceSelectors...)
+
+	sels := make([]labels.Selector, 0, len(skipLabels))
+	for _, v := range skipLabels {
+		s, err := labels.Parse(v)
+		if err == nil {
+			sels = append(sels, s)
+		}
+	}
+
+	for _, v := range namespaces.Items {
+		if slices.Contains(skipNamespaces, v.Name) ||
+			slices.ContainsFunc(sels, func(val labels.Selector) bool {
+				return val.Matches(labels.Set(v.Labels))
+			}) {
+			continue
+		}
+
+		err = k.Kube.CoreV1().Namespaces().Delete(ctx, v.Name, *metav1.NewDeleteOptions(0))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func (k *KubeSuite) TearDownSuite() {
+	ctx := context.Background()
+	if k.Cluster != nil {
+		err := DeleteCluster(ctx, k.Cluster)
+		if err != nil {
+			fmt.Println("Failed to clean up cluster.", err.Error())
+		}
+	}
+}
+
+func (k *KubeSuite) SetupSuite() {
+	ctx := context.Background()
+	var err error
+	k.Cluster, err = CreateCluster(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	k.Config, err = clientcmd.BuildConfigFromFlags("", k.Cluster.KubeConfPath)
+	if err != nil {
+		panic(err)
+	}
+
+	k.Kube, err = kubernetes.NewForConfig(k.Config)
+	if err != nil {
+		panic(err)
+	}
+
+	_ = os.Setenv(clientcmd.RecommendedConfigPathEnvVar, k.Cluster.KubeConfPath)
+}
